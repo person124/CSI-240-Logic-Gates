989089867c9d7fad842506a42b5713accd3a5880
Matthew James Harrison <mjsonofharry@gmail.com>
2016-04-21 21:54:37 -0400

    Fixed the header file
    it got WEIRD guys ok

--- header.h
+++ header.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef HEADER_H
 #define HEADER_H
 
@@ -11,7 +10,7 @@
 #include "grid.h"
 #include "layer.h"
 #include "component.h"
-#include "start.h"
+#include "power.h"
 #include "wire.h"
 #include "light.h"
 
@@ -91,90 +90,4 @@ Command     getCommand();
 CommandType getCommandType(stringstream &ss);
 int         getPosition(stringstream &ss, const int bound);
 
-=======
-#ifndef HEADER_H
-#define HEADER_H
-
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include "Utilities.h"
-
-using namespace std;
-
-const int    COMMAND_SIZE = 7;
-const string COMMAND_NAME[COMMAND_SIZE]
-{
-    "Add",
-    "Remove",
-    "Up",
-    "Down",
-    "Run",
-    "Clear",
-    "Quit"
-};
-enum CommandType
-{
-    /* Valid as input */
-    ADD,
-    REMOVE,
-    UP,
-    DOWN,
-    RUN,
-    CLEAR,
-    QUIT,
-
-    /* Invalid as input */
-    NONE
-};
-
-const int    COMPONENT_SIZE = 3;
-const string COMPONENT_NAME[COMPONENT_SIZE]
-{
-    "Power",
-    "Wire",
-    "Light",
-};
-enum ComponentType
-{
-    /* Valid as input */
-    POWER,
-    WIRE,
-    LIGHT,
-
-    /* Invalid as input */
-    EMPTY
-};
-
-struct Command
-{
-    CommandType   mType;
-    ComponentType mComponent;
-    int           mParameter1;
-    int           mParameter2;
-
-    Command()
-    {
-        mType       = NONE;
-        mComponent  = EMPTY;
-        mParameter1 = -1;
-        mParameter2 = -1;
-    }
-};
-
-void addComponent(const Command command);
-void displayGrid();
-void displayCommands();
-void displayComponents();
-void layerUp(const Command command, const int &layer);
-void layerDown(const Command command, const int &layer);
-void removeComponent(const Command command);
-
-Command     getCommand();
-CommandType getCommandType(stringstream &ss);
-ComponentType getComponentType(stringstream &ss);
-int         getPosition(stringstream &ss, const int bound);
-
->>>>>>> origin/master
 #endif
